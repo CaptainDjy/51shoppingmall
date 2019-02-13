fef433bff358c6f4bf108a5d050cee6d7c45be9a
杜金永 <1533036253@qq.com>
2019-02-13 13:28:04 +0800

    Create searchCon.php

---
+++ searchCon.php
@@ -0,0 +1,10 @@
+<div class="col-xs-12 col-md-3 pull-right col-sm-6">
+  <div class="head-search">
+    <div>
+      <form action="search_result.php" method="post" class="form-inline form-search">
+        <input name="searchword" maxlength="100" class="search-query" type="search" placeholder="请输入......">
+        <input value="搜索" type="submit">
+      </form>
+    </div>
+  </div>
+</div>
